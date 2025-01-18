67f1936fbbaae0e950f0f71d1b8b8c80bde5397f
rini <rini@rinici.de>
2025-01-18 14:30:46 -0300

    child processes?????/

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,45 @@
 pub fn main() !void {
-    const pid = c.fork();
+    const child = c.fork();
 
-    if (pid == 0) {
-        ptrace(c.PTRACE_TRACEME, 0, null, null);
+    if (child == 0) {
+        ptrace(c.PTRACE_TRACEME, 0, 0, 0);
+        _ = c.raise(c.SIGSTOP);
         _ = c.execvp(std.os.argv[1], std.os.argv[1..].ptr);
         unreachable;
     }
 
+    var pid: c.pid_t = undefined;
     var buf: [4096]u8 = undefined;
     var regs: c.user_regs_struct = undefined;
+    var status: c_int = undefined;
     var in_syscall = false;
 
-    while (!c.WIFEXITED(wait())) {
-        ptrace(c.PTRACE_GETREGS, pid, null, &regs);
+    _ = c.wait(null);
+
+    const opts = c.PTRACE_O_TRACEFORK |
+        c.PTRACE_O_TRACEVFORK |
+        c.PTRACE_O_TRACECLONE;
+
+    ptrace(c.PTRACE_SETOPTIONS, child, 0, opts);
+    ptrace(c.PTRACE_CONT, child, 0, 0);
+
+    while (true) {
+        pid = c.wait(&status);
+        if (pid == 0 or (c.WIFEXITED(status) and pid == child))
+            break;
+        if (c.WIFSIGNALED(status) and c.WSTOPSIG(status) != c.SIGTRAP)
+            continue;
+
+        ptrace(c.PTRACE_GETREGS, pid, 0, @intFromPtr(&regs));
 
         if (!in_syscall) switch (regs.orig_rax) {
-            c.SYS_openat => std.debug.print("openat '{s}' = {}\n", .{ peekString(pid, regs.rsi, &buf), regs.rax }),
+            c.SYS_openat => std.debug.print("[pid {}] openat '{s}' = {}\n", .{
+                pid, peekString(pid, regs.rsi, &buf), regs.rax,
+            }),
             else => {},
         };
 
-        ptrace(c.PTRACE_SYSCALL, pid, null, null);
+        ptrace(c.PTRACE_SYSCALL, pid, 0, 0);
         in_syscall = !in_syscall;
     }
 }
@@ -28,32 +48,24 @@ fn peekString(pid: c_int, addr: usize, buf: []u8) []u8 {
     var i: usize = 0;
     while (true) : (i += @sizeOf(usize)) {
         const word = buf[i .. i + @sizeOf(usize)];
-        ptrace(c.PTRACE_PEEKDATA, pid, @ptrFromInt(addr + i), (word.ptr));
+        ptrace(c.PTRACE_PEEKDATA, pid, addr + i, @intFromPtr(word.ptr));
 
         if (std.mem.indexOfScalar(u8, word, 0)) |j|
             return buf[0 .. i + j];
     }
 }
 
-fn ptrace(comptime op: c_int, pid: c.pid_t, addr: ?*anyopaque, data: ?*anyopaque) void {
-    const res = linux.syscall4(
-        .ptrace,
-        op,
-        @intCast(pid),
-        @intFromPtr(addr),
-        @intFromPtr(data),
-    );
-    std.debug.assert(res == 0);
-}
-
-fn wait() c_int {
-    var status: c_int = undefined;
-    _ = c.wait(&status);
-    return status;
+fn ptrace(comptime op: c_int, pid: c.pid_t, addr: usize, data: usize) void {
+    const result = linux.syscall4(.ptrace, op, @intCast(pid), addr, data);
+    if (@as(isize, @bitCast(result)) < 0) {
+        c.perror("ptrace");
+        unreachable;
+    }
 }
 
 const c = @cImport({
     @cInclude("fcntl.h");
+    @cInclude("stdio.h");
     @cInclude("unistd.h");
     @cInclude("sys/ptrace.h");
     @cInclude("sys/reg.h");
@@ -64,4 +76,4 @@ const c = @cImport({
 const std = @import("std");
 const builtin = @import("builtin");
 const linux = std.os.linux;
-const SYS = linux.SYS;
+const assert = std.debug.assert;
