99c188f675d6294ca6958f39c651951091d76f9c
rini <rini@rinici.de>
2025-02-07 01:59:51 -0300

    waaa

--- watchtrace.c
+++ watchtrace.c
@@ -13,32 +13,64 @@
 
 #define BYTEC sizeof(long long /* maaan */)
 
+typedef struct {
+  pid_t pid;
+  bool in_syscall;
+} proc;
+
 pid_t child;
+size_t nprocs = 0;
+proc *procmap = 0;
 
 int ptstrncpy(pid_t pid, char *dst, long long src, size_t nbytes);
 int ptresolvefd(pid_t pid, char *dst, int fd);
 
-void handle_access(pid_t pid, int dir, long long name, int writable, int fd) {
-  if (writable)
+proc *getproc(pid_t pid) {
+  for (int i = 0; i < nprocs; i++)
+    if (pid == procmap[i].pid)
+      return &procmap[i];
+
+  printf("[pid %i] alloc\n", pid);
+
+  procmap = reallocarray(procmap, ++nprocs, sizeof(proc));
+  procmap[nprocs - 1].pid = pid;
+  procmap[nprocs - 1].in_syscall = false;
+
+  return &procmap[nprocs - 1];
+}
+
+void handle_access(proc *proc, int dir, size_t name, int mode, int ret) {
+  if (mode & W_OK)
     return;
 
   char buf[PATH_MAX];
-  ptstrncpy(pid, buf, name, PATH_MAX);
+  ptstrncpy(proc->pid, buf, name, PATH_MAX);
 
-  printf("[pid %i] access '%s' in %d = %d\n", pid, buf, dir, fd);
-  if (fd < 0)
-    printf("[pid %i] %s\n", pid, strerror(-fd));
+  printf("[pid %i] access '%s' in %d = %d\n", proc->pid, buf, dir, ret);
+  if (ret < 0)
+    printf("[pid %i] %s\n", proc->pid, strerror(-ret));
 }
 
-void handle_close(pid_t pid, int fd, int ret) {
-  if (ret == 0)
-    printf("[pid %i] close %d\n", pid, fd);
+void handle_open(proc *proc, int dir, size_t name, int flags, int ret) {
+  if (flags & (O_RDWR | O_WRONLY))
+    return;
+
+  char buf[PATH_MAX];
+  ptstrncpy(proc->pid, buf, name, PATH_MAX);
+
+  printf("[pid %i] open '%s' in %d = %d\n", proc->pid, buf, dir, ret);
+  if (ret < 0)
+    printf("[pid %i] %s\n", proc->pid, strerror(-ret));
 }
 
-bool in_syscall = false;
+void handle_close(proc *proc, int fd) {
+  printf("[pid %i] close %d\n", proc->pid, fd);
+}
 
 void handle_syscall(pid_t pid) {
-  if ((in_syscall = !in_syscall))
+  proc *proc = getproc(pid);
+
+  if ((proc->in_syscall = !proc->in_syscall))
     return;
 
   struct user_regs_struct regs;
@@ -46,22 +78,34 @@ void handle_syscall(pid_t pid) {
 
   // this bit is arch-dependent, syscall(2)
   switch (regs.orig_rax) {
-  case SYS_access:
-    handle_access(pid, AT_FDCWD, regs.rdi, regs.rsi & W_OK, regs.rax);
-    break;
-  case SYS_faccessat:
-    handle_access(pid, regs.rdi, regs.rsi, regs.rdx & W_OK, regs.rax);
-    break;
-  case SYS_open:
-    handle_access(pid, AT_FDCWD, regs.rdi, regs.rsi & (O_WRONLY | O_RDWR), regs.rax);
-    break;
-  case SYS_openat:
-    handle_access(pid, regs.rdi, regs.rsi, regs.rdx & (O_WRONLY | O_RDWR), regs.rax);
-    break;
-  case SYS_close:
-    handle_close(pid, regs.rdi, regs.rax);
-    break;
+    case SYS_open:
+      handle_open(proc, AT_FDCWD, regs.rdi, regs.rsi, regs.rax);
+      break;
+    case SYS_openat:
+      handle_open(proc, regs.rdi, regs.rsi, regs.rdx, regs.rax);
+      break;
+    case SYS_access:
+      handle_access(proc, AT_FDCWD, regs.rdi, regs.rsi, regs.rax);
+      break;
+    case SYS_faccessat:
+      handle_access(proc, regs.rdi, regs.rsi, regs.rdx, regs.rax);
+      break;
+    case SYS_close:
+      handle_close(proc, regs.rdi);
+      break;
   }
+}
+
+void handle_exit(pid_t pid) {
+  if (pid == child)
+    exit(0);
+
+  printf("[pid %i] exit\n", pid);
+
+  int i = 0;
+  while (procmap[i++].pid != pid);
+
+  memmove(procmap + i - 1, procmap + i, nprocs - i);
 }
 
 void launch(char *argv[]) {
@@ -101,13 +145,15 @@ int main(int argc, char *argv[]) {
     int pid = wait(&status);
 
     if (WIFEXITED(status)) {
-      if (pid == child)
-        exit(0);
+      handle_exit(pid);
       continue;
     }
 
-    if (WIFSTOPPED(status) && WSTOPSIG(status) == (SIGTRAP | 0x80))
-      handle_syscall(pid);
+    if (WIFSTOPPED(status)) switch (WSTOPSIG(status)) {
+      case SIGTRAP | 0x80:
+        handle_syscall(pid);
+        break;
+    }
 
     ptrace(PTRACE_SYSCALL, pid, 0, 0);
   }
